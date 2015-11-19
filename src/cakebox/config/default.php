e615e3a2845bbfc98b579a3d9b25ae0d0ae6a705
Morgan AUCHEDE <morgan.auchede@gmail.com>
2015-11-19 13:06:03 +0100

    Initial commit.

---
+++ src/cakebox/config/default.php
@@ -0,0 +1,20 @@
+<?php
+
+$app['cakebox.root'] = '%CAKEBOX_ROOT%';
+$app['cakebox.access'] = '%CAKEBOX_ACCESS%';
+$app['cakebox.language'] = '%CAKEBOX_LANGUAGE%';
+
+$app['directory.ignoreDotFiles'] = filter_var('%DIRECTORY_IGNORE_DOTFILES%', FILTER_VALIDATE_BOOLEAN);
+$app['directory.ignore'] = '%DIRECTORY_IGNORE%';
+
+$app['player.default_type'] = '%PLAYER_DEFAULT_TYPE%';
+$app['player.auto_play'] = '%PLAYER_AUTO_PLAY%';
+
+$app['rights.canPlayMedia'] = filter_var('%RIGHTS_CAN_PLAY_MEDIA%', FILTER_VALIDATE_BOOLEAN);
+$app['rights.canDownloadFile'] = filter_var('%RIGHTS_CAN_DOWNLOAD_FILE%', FILTER_VALIDATE_BOOLEAN);
+$app['rights.canArchiveDirectory'] = filter_var('%RIGHTS_CAN_ARCHIVE_DIRECTORY%', FILTER_VALIDATE_BOOLEAN);
+$app['rights.canDelete'] = filter_var('%RIGHTS_CAN_DELETE%', FILTER_VALIDATE_BOOLEAN);
+
+$app['bs.login'] = '%BS_LOGIN%';
+$app['bs.passwd'] =  '%BS_PASSWORD%';
+$app['bs.apikey'] = '%BS_API_KEY%';
