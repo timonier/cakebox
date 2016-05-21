c409a1fdd859e87d5eab5d9ae17018472fcf98f3
Morgan Auchede <morgan.auchede@gmail.com>
2016-05-21 20:48:48 +0200

    Improve usage of s6-overlay.

--- src/rootfs/etc/cont-init.d/03-configure-php5-fpm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv /bin/sh
-
-sed -i "s@%CAKEBOX_USER%@${CAKEBOX_USER:-"nobody"}@g" /etc/php5/fpm/pool.d/www.conf
-sed -i "s@%CAKEBOX_GROUP%@${CAKEBOX_GROUP:-"nogroup"}@g" /etc/php5/fpm/pool.d/www.conf
-
-exit 0
